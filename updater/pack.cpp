3e61ace0e5a6ae39fc3841ad07c00fee1289dfe9
Thomas Epperson <thomas.epperson@gmail.com>
2011-07-24 09:49:28 -0500

    Testing for pack loading.

--- updater/pack.cpp
+++ updater/pack.cpp
@@ -38,30 +38,54 @@ int pack::load_index(int encrypted)
 		printf("Attempting to decrypt\n");
 		decrypt((unsigned char*)files, (unsigned char*)files, num_files * sizeof(file_entry));
 //		crypt.decrypt_block((unsigned char*)files, num_files * sizeof(file_entry));
-		for (int i = 0; i < 20; i++)
-		{
-			if (files[i].name[19] != 0)
-			{
-				for (int j = 0; j < 20; j++)
-				{
-					printf("%02x:", files[i].name[j] & 0xFF);
-				}
-				printf("\n\t");
-				printf(" length %08x @ %08x\n", 
-					files[i].size, files[i].offset);
-			}
-			else
-			{
-				printf("%s length %08x @ %08x\n", 
-					files[i].name, files[i].size, files[i].offset);
-			}
-		}
 	}
 	if (files[0].offset != 0)
 	{	//fix the first offset
 		printf("Fixing offset of the first file\n");
 		files[0].offset = 0;
 	}
+	//also check the size of the last file
+	
+	for (int i = num_files-20; i < num_files; i++)
+	{
+		if (files[i].name[19] != 0)
+		{
+			for (int j = 0; j < 20; j++)
+			{
+				printf("%02x:", files[i].name[j] & 0xFF);
+			}
+			printf("\n\t");
+			printf(" length %08x @ %08x\n", 
+				files[i].size, files[i].offset);
+		}
+		else
+		{
+			printf("%s length %08x @ %08x\n", 
+				files[i].name, files[i].size, files[i].offset);
+		}
+	}
+#if 1
+	open_data();	//open the data file it is not already
+	int num_invalid = 0;
+	fseek(data_buf, 0, SEEK_END);
+	int size_data = ftell(data_buf);
+	fseek(data_buf, 0, SEEK_SET);
+	
+	for (int i = 0; i < num_files; i++)
+	{
+		if (files[i].offset >= size_data)
+		{
+			num_invalid++;
+		}
+		else if ((files[i].offset + files[i].size) > size_data)
+		{
+			num_invalid++;
+		}
+		//check offset is valid
+		//size+offset not too large
+	}
+	printf("There were %d invalid file entries detected\n", num_invalid);
+#endif
 	return 0;
 }
 
@@ -101,7 +125,7 @@ int pack::detect_dupes()	//detects duplicate files
 			}			
 		}
 	}
-	if (dupes > 0)
+	if (dupes >= 0)
 	{
 		printf("\tThere were %d duplicate files detected\n", dupes);
 	}
@@ -124,14 +148,21 @@ int pack::load_data(int encrypted)
 	{
 		printf("Data will not be decrypted\n");
 	}
-	data_buf = fopen(data_file, "rb");
+	open_data();
 	file_data = new char*[num_files];
 	for (int i = 0; i < num_files; i++)
 	{
 		//check for actual file size
-		fseek(data_buf, files[i].offset, SEEK_SET);
-		file_data[i] = new char[files[i].size];
-		fread(file_data[i], 1, files[i].size, data_buf);
+		if (fseek(data_buf, files[i].offset, SEEK_SET) != 0)
+		{
+			printf("Error seeking to file offset\n");
+			file_data[i] = 0;
+		}
+		else
+		{
+			file_data[i] = new char[files[i].size];
+			fread(file_data[i], 1, files[i].size, data_buf);
+		}
 	}
 	return 0;
 }
@@ -164,6 +195,12 @@ pack::pack(const char *name, int encrypted)
 //	crypt.initialize(key);
 	load_index(encrypted);
 	load_data(encrypted);
+}
+
+void pack::open_data()
+{
+	if (data_buf == 0)
+		data_buf = fopen(data_file, "rb");
 }
 
 pack::~pack()
