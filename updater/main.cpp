bc6e03f1c53c30d76776e6aeb76b7d8a4c0960a1
Thomas Epperson <thomas.epperson@gmail.com>
2011-08-18 15:24:37 -0500

    Test code for SDL_mixer.
    Signed-off-by: Thomas Epperson <thomas.epperson@gmail.com>

--- updater/main.cpp
+++ updater/main.cpp
@@ -1,9 +1,17 @@
 #include <stdio.h>
 
+#include "music.h"
 #include "sdl_master.h"
+#include "SDL_mixer.h"
 
 int main(int argc, char* argv[])
 {
+	if (SDL_Init(SDL_INIT_EVERYTHING) == -1)
+	{
+		printf("Failed to start SDL\n");
+		return 1;
+	}
+	
 	sdl_master graphics;
 	graphics.create_client();
 	graphics.process_events();
--- updater/music.h
+++ updater/music.h
@@ -1,11 +1,18 @@
 #ifndef __MUSIC_H_
 #define __MUSIC_H_
 
+#include "SDL.h"
+#include "SDL_mixer.h"
+
 class music
 {
 	public:
 		music();
 		int init();
+		
+		void start_music(const char *name);
+	private:
+		Mix_Music *cur_music;
 };
 
 #endif
