053ca8cd6ddc6d67bc9de4d81f96e3fbb5e4658e
Thomas Epperson <thomas.epperson@gmail.com>
2022-06-10 02:17:16 -0400

    Implement basic logic that checks loading of basic game resources.

--- rust/src/main.rs
+++ rust/src/main.rs
@@ -6,29 +6,53 @@ use std::time::Duration;
 use std::fs;
 
 mod settings;
-mod sprites;
-mod pack;
 
+mod sprites;
 use crate::sprites::*;
 
+mod pack;
+use crate::pack::*;
+
 enum MessageToAsync {
-	LoadResource,
+	LoadResources(String),
 }
 
 enum MessageFromAsync {
-	Resource,
+	ResourceStatus(bool),
+}
+
+async fn load_resources(packs: &mut Vec<Pack>, path: String) -> Result<(), ()> {
+	let mut pack = Pack::new(format!("{}/Text", path), false);
+	let e = pack.load().await;
+	if let Err(_a) = e {
+		return Err(());
+	}
+	Ok(())
 }
 
 async fn async_main(mut r: tokio::sync::mpsc::Receiver<MessageToAsync>,
 	mut s: tokio::sync::mpsc::Sender<MessageFromAsync>) {
 	println!("Async main");
+	
+	let mut packs: Vec<Pack> = Vec::new();
+	
 	loop {
 		let message = r.recv().await;
 		match message {
 			None => break,
 			Some(msg) => {
 				match msg {
-					MessageToAsync::LoadResource => {
+					MessageToAsync::LoadResources(path) => {
+						println!("Loading resources {}", path);
+						
+						match load_resources(&mut packs, path).await {
+							Ok(()) => {
+								s.send(MessageFromAsync::ResourceStatus(true)).await;
+							}
+							Err(()) => {
+								s.send(MessageFromAsync::ResourceStatus(false)).await;
+							}
+						}
 					}
 				}
 			}
@@ -54,12 +78,14 @@ pub fn main() {
         _ => false,
     };
     
-    let resources = settings.get("general","resources").unwrap().to_string();
+    let resources = settings.get("general","resources").unwrap();
     
     let rt = tokio::runtime::Runtime::new().unwrap();
     let (s1, r1) = tokio::sync::mpsc::channel(100);
-    let (s2, r2) = tokio::sync::mpsc::channel(100);
+    let (s2, mut r2) = tokio::sync::mpsc::channel(100);
     rt.spawn(async_main(r1, s2));
+    
+    s1.blocking_send(MessageToAsync::LoadResources(resources.clone()));
     
     println!("Loading resources from {}", resources);
     //TODO load from from Font/eng.fnt
@@ -68,6 +94,13 @@ pub fn main() {
     //TODO load tiles
     
     //let sprites = Sprites::load(".".to_string());
+
+    //load program settings, including where to find resources
+    let game_settings = settings::Settings { game_resources: "/nowhere".to_string() };
+
+    //load pack files
+    //Text, Tile, Sprite0-Sprite15
+
 
     let sdl_context = sdl2::init().unwrap();
     let video_subsystem = sdl_context.video().unwrap();
@@ -79,7 +112,7 @@ pub fn main() {
 
     if !windowed {
 	windowb = windowb.fullscreen();
-}
+    }
     let window = windowb.build().unwrap();
 
     let mut canvas = window.into_canvas().build().unwrap();
@@ -90,6 +123,45 @@ pub fn main() {
     let mut event_pump = sdl_context.event_pump().unwrap();
     let mut i = 0;
     'running: loop {
+	let mut fail = false;
+	while let Ok(msg) = r2.try_recv() {
+		match msg {
+			MessageFromAsync::ResourceStatus(b) => {
+				if !b {
+					println!("Failed to load game resources");
+					let scheme = sdl2::messagebox::MessageBoxColorScheme {
+						background: (0,0,255),
+						text: (0,0,0),
+						button_border: (0,0,0),
+						button_background: (255,255,255),
+						button_selected:(0,255,255),
+					};
+					let mut vid_win = video_subsystem.window("l1j-client", 320, 240);
+					let mut windowb = vid_win.position_centered();
+					if !windowed {
+						windowb = windowb.fullscreen();
+					}
+					let window = windowb.build().unwrap();
+					sdl2::messagebox::show_message_box(
+						sdl2::messagebox::MessageBoxFlag::ERROR,
+						&[sdl2::messagebox::ButtonData{
+							flags: sdl2::messagebox::MessageBoxButtonFlag::RETURNKEY_DEFAULT,
+							button_id: 0,
+							text: "OK",
+						}],
+						"ERROR",
+						"Unable to load game resources",
+						&window,
+						scheme,
+						);
+					fail = true;
+				}
+			}
+		}
+	}
+	if fail {
+		break
+	};
         i = (i + 1) % 255;
         canvas.set_draw_color(Color::RGB(i, 64, 255 - i));
         canvas.clear();
@@ -101,12 +173,6 @@ pub fn main() {
             }
         }
         // The rest of the game loop goes here...
-
-        //load program settings, including where to find resources
-        let game_settings = settings::Settings { game_resources: "/nowhere".to_string() };
-
-        //load pack files
-        //Text, Tile, Sprite0-Sprite15
 
         canvas.present();
         ::std::thread::sleep(Duration::new(0, 1_000_000_000u32 / 20));
--- rust/src/pack.rs
+++ rust/src/pack.rs
@@ -6,11 +6,30 @@ pub struct Pack {
 	
 }
 
+pub enum Exception {
+	IoError,
+}
+
+impl From<std::io::Error> for Exception {
+	fn from(_a: std::io::Error) -> Self {
+		Exception::IoError
+	}
+}
+
 impl Pack {
-	fn new(e: bool, n: String) -> Self {
+	pub fn new(n: String, e: bool) -> Self {
 		Self {
 			encrypted: e,
 			name: n,
 		}
+	}
+	
+	pub async fn load(&mut self) -> Result<(), Exception>{
+		println!("Opening pack {}", self.name);
+		let content = format!("{}.pak", self.name);
+		let index = format!("{}.idx", self.name);
+		let cont: tokio::fs::File = tokio::fs::File::open(content).await?;
+		let indx: tokio::fs::File = tokio::fs::File::open(index).await?;
+		Ok(())
 	}
 }
